1591e071ee9fb7bbca7fbcaf96c1594be104e263
gerard <gerard.alonso.7e9@itb.cat>
2025-12-09 12:34:22 +0100

    method and test for the program

---
+++ bin/Debug/TestWarriorMethods/Test1.cs
@@ -0,0 +1,28 @@
+﻿using WarriorMethods;
+
+namespace TestWarriorMethods
+{
+    [TestClass]
+    public sealed class TestWarriorUtils
+    {
+        [TestMethod]
+        public void TC1_VerifyStatus_MaxHP()
+        {
+            //Arrange
+            int speed, attack;
+            bool canRun;
+            string status, colour;
+
+            //act
+            WarriorUtils.CalculateWarriorStatus(100,out status, out speed, out attack, out canRun, out colour);
+
+            //assert
+
+            Assert.AreEqual("healthy", status);
+            Assert.AreEqual(100, speed);
+            Assert.AreEqual(100, attack);
+            Assert.IsTrue(canRun);
+            Assert.AreEqual("normal",colour);
+        }
+    }
+}
---
+++ bin/Debug/WarriorUtils/Class1.cs
@@ -0,0 +1,52 @@
+﻿namespace WarriorMethods
+{
+    public class WarriorUtils
+    {
+        public static void CalculateWarriorStatus(int hp, out string status,
+        out int speed, out int attack, out bool canRun, out string screenColour)
+        {
+            hp = Math.Min(hp, 100);
+
+            if (hp == 0)
+            {
+                status = "Dead";
+                speed = 0;
+                attack = 0;
+                canRun = false;
+                screenColour = "red";
+            }
+            else if (hp >= 1 && hp <= 25)
+            {
+                status = "Critical";
+                speed = 50;
+                attack = 50;
+                canRun = false;
+                screenColour = "red";
+            }
+            else if (hp >= 26 && hp <= 50)
+            {
+                status = "Severely injured";
+                speed = 70;
+                attack = 80;
+                canRun = false;
+                screenColour = "normal";
+            }
+            else if (hp >= 51 && hp <= 75)
+            {
+                status = "injured";
+                speed = 90;
+                attack = 100;
+                canRun = true;
+                screenColour = "normal";
+            }
+            else
+            {
+                status = "healthy";
+                speed = 100;
+                attack = 100;
+                canRun = true;
+                screenColour = "normal";
+            }
+        }
+    }
+}
